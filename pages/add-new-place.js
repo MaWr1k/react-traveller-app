54ac4443f5e0cbef38e4bab6cbaea7514b129665
Oleksandr <mawr1k66@gmail.com>
2022-06-27 16:28:51 +0300

    gir ignore change

---
+++ pages/add-new-place.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddNewPlace = () => {
+  return (
+    <div>
+      <h1>Add new place</h1>
+    </div>
+  );
+};
+
+export default AddNewPlace;
---
+++ pages/add-new-route.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddNewRoute = () => {
+  return (
+    <div>
+      <h1>Add new route</h1>
+    </div>
+  );
+};
+
+export default AddNewRoute;
