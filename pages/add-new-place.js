c73ab235f8333940faee06bbb53bb95f5a10bd7f
Oleksandr <mawr1k66@gmail.com>
2022-07-01 15:43:23 +0300

    input reworked

--- pages/add-new-place.js
+++ pages/add-new-place.js
@@ -1,59 +1,32 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
+
 import Input from "../components/common/form/Input";
 import useInput from "../hooks/use-input";
 
 const AddNewPlace = () => {
+  const title = useInput(value => value.length > 3);
+  const address = useInput(value => value.length > 5);
+  const image = useInput(value => value.length > 5);
+  const desc = useInput(value => value.length > 20);
 
-  const {
-    value: titleValue,
-    isValid: titleIsValid,
-    hasErrors: titleHasErrors,
-    valueInputClasses: titleInputClasses,
-    valueChangeHandler: titleChangeHandler,
-    inputBlurHandler: titleBlurHandler,
-    reset: titleReset
-  } = useInput(value => value.length > 3);
-  const {
-    value: addressValue,
-    isValid: addressIsValid,
-    hasErrors: addressHasErrors,
-    valueInputClasses: addressInputClasses,
-    valueChangeHandler: addressChangeHandler,
-    inputBlurHandler: addressBlurHandler,
-    reset: addressReset
-  } = useInput(value => value.length > 5);
-  const {
-    value: imageValue,
-    isValid: imageIsValid,
-    hasErrors: imageHasErrors,
-    valueInputClasses: imageInputClasses,
-    valueChangeHandler: imageChangeHandler,
-    inputBlurHandler: imageBlurHandler,
-    reset: imageReset
-  } = useInput(value => value.length > 5);
-  const {
-    value: descValue,
-    isValid: descIsValid,
-    hasErrors: descHasErrors,
-    valueInputClasses: descInputClasses,
-    valueChangeHandler: descChangeHandler,
-    inputBlurHandler: descBlurHandler,
-    reset: descReset
-  } = useInput(value => value.length > 20);
+  const [isSending, setIsSending] = useState(false);
+  const [isSendSuccess, setIsSendSuccess] = useState(false);
+
 
   let formIsValid = false;
-  if(titleIsValid && addressIsValid && imageIsValid && descIsValid){
+  if(title.isValid && address.isValid && image.isValid && desc.isValid){
     formIsValid = true;
   }
 
+
   const formSubmitHandler = async (e) => {
     e.preventDefault();
-
+    setIsSending(true);
     const data = {
-      title: titleValue,
-      address: addressValue,
-      image: imageValue,
-      desc: descValue,
+      title: title.value,
+      address: address.value,
+      image: image.value,
+      desc: desc.value,
     };
     const response = await fetch('/api/add-new-place',{
       method: 'POST',
@@ -68,51 +41,64 @@ const AddNewPlace = () => {
     console.log(responseData);
 
     if(formIsValid){
-      titleReset();
-      addressReset();
-      imageReset();
-      descReset();
+      title.reset();
+      address.reset();
+      image.reset();
+      desc.reset();
+      setIsSending(false);
+      setIsSendSuccess(true);
     }
+  }
+  let formContent = '';
+
+
+  if(!isSending){
+    formContent = <form action="" onSubmit={formSubmitHandler}>
+      <Input name='title'
+             placeholder='Place title'
+             value={title.value}
+             onChange={title.valueChangeHandler}
+             onBlur={title.inputBlurHandler}
+             inputClasses={title.valueInputClasses}
+             errorText='Title have to be more than 3 characters'
+             hasErrors={title.hasErrors}/>
+      <Input name='address'
+             placeholder='Place address'
+             value={address.value}
+             onChange={address.valueChangeHandler}
+             onBlur={address.inputBlurHandler}
+             inputClasses={address.valueInputClasses}
+             errorText='Address have to be more than 5 characters'
+             hasErrors={address.hasErrors}/>
+      <Input name='image'
+             placeholder='Place image'
+             value={image.value}
+             onChange={image.valueChangeHandler}
+             onBlur={image.inputBlurHandler}
+             inputClasses={image.valueInputClasses}
+             errorText='Image have to be more than 5 characters'
+             hasErrors={image.hasErrors}/>
+      <Input name='desc'
+             placeholder='Place description'
+             value={desc.value}
+             onChange={desc.valueChangeHandler}
+             onBlur={desc.inputBlurHandler}
+             inputClasses={desc.valueInputClasses}
+             errorText='Description have to be more than 20 characters'
+             hasErrors={desc.hasErrors}/>
+      <button disabled={!formIsValid}>Add new place</button>
+    </form>
+  }
+  if(isSending){
+    formContent = <p>Sending... </p>
+  }
+  if(isSendSuccess && !isSending){
+    formContent = <p>Place added successfully!</p>
   }
   return (
     <Fragment>
       <h1>Add new place</h1>
-      <form action="" onSubmit={formSubmitHandler}>
-        <Input name='title'
-               placeholder='Place title'
-               value={titleValue}
-               onChange={titleChangeHandler}
-               onBlur={titleBlurHandler}
-               inputClasses={titleInputClasses}
-               errorText='Title have to be more than 3 characters'
-               hasErrors={titleHasErrors}/>
-        <Input name='address'
-               placeholder='Place address'
-               value={addressValue}
-               onChange={addressChangeHandler}
-               onBlur={addressBlurHandler}
-               inputClasses={addressInputClasses}
-               errorText='Address have to be more than 5 characters'
-               hasErrors={addressHasErrors}/>
-        <Input name='image'
-               placeholder='Place image'
-               value={imageValue}
-               onChange={imageChangeHandler}
-               onBlur={imageBlurHandler}
-               inputClasses={imageInputClasses}
-               errorText='Image have to be more than 5 characters'
-               hasErrors={imageHasErrors}/>
-        <Input name='desc'
-               placeholder='Place description'
-               value={descValue}
-               onChange={descChangeHandler}
-               onBlur={descBlurHandler}
-               inputClasses={descInputClasses}
-               errorText='Description have to be more than 20 characters'
-               hasErrors={descHasErrors}/>
-        <button disabled={!formIsValid}>Add new place</button>
-      </form>
-
+      {formContent}
     </Fragment>
   );
 };
